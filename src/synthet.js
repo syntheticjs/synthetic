3fa02adc03f37c2b198c19994d9e56933b049f2d
Morulus <vladimirmorulus@gmail.com>
2015-07-12 00:29:57 +0300

    removed WebModule and adds webModule to each new element of component. Fixed major bugs in IE and Safari. Required new version of querySelector and Mutagen.

--- src/synthet.js
+++ src/synthet.js
@@ -12,107 +12,98 @@ AMD Synthet
 	"polyvitamins~polyscope@master/gist/convert/camelize.js",
 	"./d3party/WebReflection/document-register-element.amd.js"
 ], function(mutagen, inherit, eventsClass, camelize) {
-	var cultAttrs = function(component) {
-		/*
-		Культивируем аттрибуты
-      	*/
-      	component.attributes = {};
-      	for (var z=0;z<this.attributes.length;z++) {
-      		component.attributes[camelize(this.attributes[z].name)] = this.attributes[z].value;
-		}
-	},Component = inherit(function(name) {
-		this.name = name;
-		this.$ = null;
-		this.template = '';
-		this.attributes = {};
-	}, eventsClass);
-	/*
-	Устанавливает шаблон для элемента
-	*/
-	Component.prototype.setTemplate = function(template) {
-		this.template = template;
-		this.on('created', function(element) {
-			mutagen.call(this.template, element);
-		});
-	}
-	/*
-	Регистриует элемент в документе
-	*/
-	Component.prototype.register = function() {
-		var component = this;
-		/*
-		Регистрируем касмотный элемент через полифил
-		*/
-		document.registerElement(this.name, {
-		    prototype: Object.create(
-		      HTMLElement.prototype, {
-		      query: {
-		      	value: function(queryString) {
-		      		var nodeList = mutagen.query(queryString, this);
-		      		if (nodeList instanceof NodeList) {
-		      			return Array.prototype.slice.apply(nodeList);
-		      		} else {
-		      			return [];
-		      		}
-		      	}
-		      },
-		      createdCallback: {value: function() {
-		      	component.$ = this;
-		      	component.$.module = component;
-		      	/*
-				Культивируем аттрибуты
-		      	*/
-		      	cultAttrs.call(this, component);
-		      	component.trigger('created',[this]);
-		      }},
-		      attachedCallback: {value: function() {
-		        component.trigger('attached',[this]);
-		      }},
-		      detachedCallback: {value: function() {
-		        component.trigger('detached',[this]);
-		      }},
-		      attributeChangedCallback: {value: function(
-		        name, previousValue, value
-		      ) {
-		      	/*
-				Культивируем аттрибуты
-		      	*/
-		      	cultAttrs.call(this, component);
-		      	component.trigger('attributeChanged',[name, previousValue, value]);
-		      }}
-		    })
-		});
-	}
-
-	var Synthet = function() {
-
-	};
-
-	Synthet.prototype = {
-		construct: Synthet
-	}
-
-	Synthet.newComponent = function(name, template, prototype) {
-		/*
-		Сбрасываем ошибку, если в имени нету деффиса
-		*/
-		if (name.indexOf('-')<0) throw 'Component name must have `-` symbol';
-		/*
-		Создаем новый компонент
-		*/
-		var WebModule = function() {
-
-		}
-		WebModule.prototype = prototype||{};
-		WebModule.prototype.constructor = WebModule;
-		WebModule = inherit(WebModule, Component);
-		var component = new WebModule(name);
-		component.setTemplate(template);
-
-		return component;
-	}
-
-	if (window) window.Synthet = Synthet;
-	return Synthet;
+	var customeElement = inherit(function(element, component) {
+            this.$ = element;
+            this.component = component;
+            this.attributes = {};
+            this.cultAttrs();
+        }, eventsClass);
+        customeElement.prototype.query = function(queryString) {
+            var nodeList = mutagen.query(queryString, this.$);
+            if (nodeList instanceof NodeList || nodeList instanceof Array) {
+                return Array.prototype.slice.apply(nodeList);
+            } else {
+                return [];
+            }
+        }
+        customeElement.prototype.cultAttrs = function() {
+           this.attributes = {};
+            for (var z = 0; z < this.$.attributes.length; z++) {
+                this.attributes[camelize(this.$.attributes[z].name)] = this.$.attributes[z].value;
+            }
+        }
+        var Module = inherit(function(name) {
+            this.name = name;
+            this.template = "";
+            this.elementPrototype = {};
+        }, eventsClass);
+        Module.prototype.setTemplate = function(template) {
+            this.template = template;
+            this.on("created", function(module) {
+                mutagen.call(this.template, module.$);
+            });
+        };
+        Module.prototype.setElementPrototype = function(proto) {
+            this.elementPrototype = proto;
+        };
+        Module.prototype.register = function() {
+            var component = this;
+           
+            document.registerElement(this.name, {
+                prototype: Object.create(HTMLElement.prototype, {
+                    
+                    createdCallback: {
+                        value: function() {
+                            var WebModule = function() {};
+                            
+                            WebModule.prototype = component.elementPrototype;
+                            WebModule.prototype.constructor = WebModule;
+                            WebModule = inherit(WebModule, customeElement);
+                            var ce = new WebModule(this, component);
+                            this.module = ce;
+                            
+                            component.trigger("created", [ ce ]);
+                        }
+                    },
+                    attachedCallback: {
+                        value: function() {
+                            component.trigger("attached", [ this.module ]);
+                        }
+                    },
+                    detachedCallback: {
+                        value: function() {
+                            component.trigger("detached", [ this.module ]);
+                        }
+                    },
+                    attributeChangedCallback: {     
+                        configurable: true,         
+                        writable: true,
+                        enumerable: true,         
+                        value: function(name, previousValue, value) {
+                            this.module.attributes[name]=value;
+                            component.trigger("attributeChanged", [ this.module, name, previousValue, value ]);
+                        }
+                    }
+                })
+                
+            });
+        };
+        var Synthet = function() {};
+        Synthet.prototype = {
+            construct: Synthet
+        };
+        Synthet.newComponent = function(name, template, elementPrototype) {
+            if (name.indexOf("-") < 0) throw "Module name must have `-` symbol";
+            /*var WebModule = function() {};
+            WebModule.prototype = prototype || {};
+            WebModule.prototype.constructor = WebModule;
+            WebModule = inherit(WebModule, Module);*/
+            var component = new Module(name);
+            component.setTemplate(template);
+            component.setElementPrototype(elementPrototype||{});
+            return component;
+        };
+        if (window) window.Synthet = Synthet;
+        return Synthet;
 
 });
