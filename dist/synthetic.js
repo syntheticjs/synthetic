f0d03e8efbd412c4b797a0f2d63aec42568a32da
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-02-02 23:06:05 +0300

    build 0.1.6

--- dist/synthetic.js
+++ dist/synthetic.js
@@ -613,7 +613,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 				aClass.apply(this, args);
 			}
-			Mixin.prototype = Object.create(superprototype,{
+			Mixin.prototype = Object.create(superprototype, {
 				
 				/*
 				Для быстрого кроссбраузерного доступа к суперпроототипу будет использоваться свойство __super__
@@ -4779,12 +4779,20 @@ return /******/ (function(modules) { // webpackBootstrap
 	    if ("undefined"===typeof this.$$childScopes) this.$$childScopes = [];
 	}.proto({
 	    /*
-	    Creates new scope
+	    Creates new scope.
+	    If prototype is function, use second argument to specify constructor arguments in array
 	    */
-	    $newScope: function(prototype) {
+	    $newScope: function(prototype, args) {
 	        var childScope;
 	        if ("function"===typeof prototype) {
-	            childScope = new (inherit(function() { }, [prototype, Scope]))(this);
+	            var superClass = (inherit(function() { }, [prototype, Scope]));
+
+	            superClass.__disableContructor__ = true;
+	        
+	            var module = new superClass();
+	            superClass.apply(module, ([this]).concat(args||[]));
+	            
+	            childScope = module;
 	        } else if ("object"===typeof prototype) {
 	            childScope = charge(prototype, Scope, [this]);
 	        } else {
